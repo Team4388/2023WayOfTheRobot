01973134339624293f132dd3ce66df284fbbfe1c
aarav18 <aaravshah18@gmail.com>
2023-01-15 20:25:07 -0700

    odometry

--- src/main/java/frc4388/robot/commands/AutoBalance.java
+++ src/main/java/frc4388/robot/commands/AutoBalance.java
@@ -10,7 +10,7 @@ import frc4388.robot.Robot;
 // NOTE:	Consider using this command inline, rather than writing a subclass.	For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoBalance extends PelvicInflamitoryDisease {
+public class AutoBalance extends PelvicInflammatoryDisease {
 	Robot.MicroBot bot;
 
 	/** Creates a new AutoBalanceTF2. */
