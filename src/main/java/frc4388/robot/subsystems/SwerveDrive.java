d0b94e7e945ed8ff106fb91ac39a714c18bf1307
aarav18 <aaravshah18@gmail.com>
2023-01-20 17:50:24 -0700

    yes

--- src/main/java/frc4388/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc4388/robot/subsystems/SwerveDrive.java
@@ -144,6 +144,7 @@ public class SwerveDrive extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    updateOdometry();
   }
 
   /**
