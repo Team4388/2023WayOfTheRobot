e0eb4e3ea9ae461c614a2ed6c993227a99c8e873
Astatin3 <mj@mikovsky.com>
2023-01-23 19:53:30 -0700

    Development

---
+++ src/main/java/frc4388/robot/subsystems/Claw.java
@@ -0,0 +1,17 @@
+public class claw {
+    // Opens claw
+    public void funtion openClaw() {
+        // Open claw
+    }
+
+    // Closes claw
+    public void function closeClaw() {
+        //Close claw
+    }
+
+    // Rotate wrist
+    public int function rotClaw(int rot) {
+        //Rotate wrist to ROT
+        return newRot;
+    }
+}
