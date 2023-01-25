0a07756c29afcf4165c0698e624210e4c3ee1b23
Aarav Shah <aaravshah18@gmail.com>
2023-01-24 18:55:04 -0700

    Renamed constants using convention

--- src/main/java/frc4388/robot/Constants.java
+++ src/main/java/frc4388/robot/Constants.java
@@ -24,8 +24,8 @@ public final class Constants {
         public static final int SMARTDASHBOARD_UPDATE_FRAME = 2;
     }
     public static final class ArmConstants {
-        public static final int minArmLen = 0;
-        public static final int maxArmLen = 1;
+        public static final int MIN_ARM_LEN = 0;
+        public static final int MAX_ARM_LEN = 1;
         public static final int SMARTDASHBOARD_UPDATE_FRAME = 2;
     }
     public static final class LEDConstants {
