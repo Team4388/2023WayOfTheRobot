67f1516d680e93455851ca657667ae5a3bc7acd6
Aarav Shah <aaravshah18@gmail.com>
2023-02-27 18:42:27 -0700

    fixed local var visibility issue

--- src/main/java/frc4388/robot/subsystems/Location.java
+++ src/main/java/frc4388/robot/subsystems/Location.java
@@ -5,8 +5,8 @@ import frc4388.robot.subsystems.Apriltags;
 public class Location {
     final Apriltags Apriltag = new Apriltags();
 
-    public boolean isLimelight = false;
-    public boolean isApriltag = false;
+    private boolean isLimelight = false;
+    private boolean isApriltag = false;
 
     //Determines which source to get pos and rot from and also resets
     public void reoderPrio(){
