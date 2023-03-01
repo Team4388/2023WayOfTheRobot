e8db8770c50967e275341834259382ded7497b94
Astatin3 <mj@mikovsky.com>
2023-02-28 17:05:18 -0700

    code review

--- src/main/java/frc4388/robot/subsystems/Apriltags.java
+++ src/main/java/frc4388/robot/subsystems/Apriltags.java
@@ -9,41 +9,28 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Apriltags {
     public static class Tag {
-        boolean visible = true;
-        double x, y, z = 0;
-    }
-    
-    public Object[] getApriltagPosition() {
-        final var tagTable = NetworkTableInstance.getDefault().getTable("apriltag");
-        final var tag = new Tag();
-        // integrate the tag system
-        
-        // return new Object[] {true, 
-        //     tagTable.getEntry("TagPosX"),
-        //     tagTable.getEntry("TagPosY"),
-        //     tagTable.getEntry("TagPosZ")
-        // };
-
-        return new Object[] {true, 
-            1,
-            2,
-            3
-        };
+        public boolean visible = true;
+        public double x,  y,  z  = 0;
+        public double ry, rp, rr = 0;
     }
 
-    public Object[] getApriltagRotation() {
+    public Tag getTagPosRot() {
         final var tagTable = NetworkTableInstance.getDefault().getTable("apriltag");
 
-        return new Object[] {true, 
-            tagTable.getEntry("TagRotY"),
-            tagTable.getEntry("TagRotP"),
-            tagTable.getEntry("TagRotR")
-        };
+        final Tag tag = new Tag();
+        tag.visible = isAprilTag();
+        tag.x  = tagTable.getEntry("TagPosX").getDouble(0);
+        tag.y  = tagTable.getEntry("TagPosY").getDouble(0);
+        tag.z  = tagTable.getEntry("TagPosZ").getDouble(0);
+        tag.ry = tagTable.getEntry("TagRotY").getDouble(0);
+        tag.rp = tagTable.getEntry("TagRotP").getDouble(0);
+        tag.rr = tagTable.getEntry("TagRotR").getDouble(0);
+
+        return tag;
     }
 
     public boolean isAprilTag() {
         final var tagTable = NetworkTableInstance.getDefault().getTable("apriltag");
-        // return tagTable.getEntry("IsTag").getBoolean(false);
-        return true;
+        return tagTable.getEntry("IsTag").getBoolean(false);
     }
 }
