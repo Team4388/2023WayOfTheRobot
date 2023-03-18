a3bc7b56b953099d6f4d80505680b74d7075a648
aarav18 <aaravshah18@gmail.com>
2023-03-18 14:41:01 -0600

    aprilrotalign works

--- src/main/java/frc4388/robot/commands/Placement/AprilRotAlign.java
+++ src/main/java/frc4388/robot/commands/Placement/AprilRotAlign.java
@@ -4,28 +4,23 @@
 
 package frc4388.robot.commands.Placement;
 
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.math.geometry.Translation2d;
-import frc4388.robot.Constants.VisionConstants;
 import frc4388.robot.commands.PelvicInflammatoryDisease;
 import frc4388.robot.subsystems.Limelight;
 import frc4388.robot.subsystems.SwerveDrive;
 
-public class LimeAlign extends PelvicInflammatoryDisease {
-
+public class AprilRotAlign extends PelvicInflammatoryDisease {
+  
   SwerveDrive drive;
   Limelight lime;
-
-  DoubleSupplier ds;
-
-  public LimeAlign(SwerveDrive drive, Limelight lime, DoubleSupplier ds, double tolerance) {
-    super(0.4, 0.4, 0.0, 0.0, tolerance);
-
+  
+  /** Creates a new AprilRotAlign. */
+  public AprilRotAlign(SwerveDrive drive, Limelight lime) {
+    super(0.1, 0.2, 0.0, 0.0, 0.0);
+  
     this.drive = drive;
     this.lime = lime;
-    this.ds = ds;
-    
+
     addRequirements(drive, lime);
   }
 
@@ -34,8 +29,7 @@ public class LimeAlign extends PelvicInflammatoryDisease {
     double err = 0.0;
 
     try {
-      System.out.println(ds.getAsDouble());
-      err = ds.getAsDouble() / (VisionConstants.H_FOV / 2);
+      err = lime.getAprilSkew();
     } catch (NullPointerException ex) {}
     
     return err;
@@ -43,6 +37,6 @@ public class LimeAlign extends PelvicInflammatoryDisease {
 
   @Override
   public void runWithOutput(double output) {
-    drive.driveWithInput(new Translation2d(output, 0.0), new Translation2d(0.0, 0.0), true);
+    drive.driveWithInput(new Translation2d(0.0, 0.0), new Translation2d(-output, 0.0), true);
   }
 }
