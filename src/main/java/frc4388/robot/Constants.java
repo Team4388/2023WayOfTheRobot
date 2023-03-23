db14e87ddd7d6909c3d79ea6bae17c1366c8f933
aarav18 <aaravshah18@gmail.com>
2023-03-22 20:02:42 -0600

    import cleanup

--- src/main/java/frc4388/robot/Constants.java
+++ src/main/java/frc4388/robot/Constants.java
@@ -7,14 +7,9 @@
 
 package frc4388.robot;
 
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
 import frc4388.utility.Gains;
 import frc4388.utility.LEDPatterns;
-import frc4388.utility.RobotUnits;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
